0d69331e1ab6bf7132be0d9b4c5c87ab7b7973e5
Kevin· <kevinnl5432@gmail.com>
2025-02-19 10:13:24 +0800

    style: update style od input box for addding task and task list

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "../styles/Todos.css";
+
+type KeyBoardEventType = React.KeyboardEvent<HTMLInputElement>;
+type PageEventType = React.MouseEvent<HTMLDivElement>;
 
 export default function Todos({
   category,
@@ -11,37 +14,47 @@ export default function Todos({
   onAddTodo: (category: string, todo: string) => void;
 }) {
   const [newTodo, setNewTodo] = useState("");
+  const addTaskRef = useRef(null);
 
-  function handleAddTodo() {
-    if (newTodo.trim()) {
+  function handleAddTodo(e: KeyBoardEventType) {
+    if (e.key === "Enter" && newTodo.trim()) {
       onAddTodo(category, newTodo);
       setNewTodo("");
     }
   }
 
+  function handlePageClick(e: PageEventType) {
+    
+  }
+
   return (
-    <>
-      <div className="todo-list">
-        <h1 className="title">{category}</h1>
+    <div className="todo-list" onClick={(e) => handlePageClick(e)}>
+      <h1 className="title">{category}</h1>
+      <div className="add-todo">
+        <input
+          type="text"
+          value={newTodo}
+          placeholder="Add Task"
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyDown={(e) => handleAddTodo(e)}
+          ref={addTaskRef}
+        />
+      </div>
+      <div className="todos">
         <ul>
           {todos.map((todo: string, index: number) => (
-            <li key={index}>{todo}</li>
+            <li key={index}>
+              <input
+                type="checkbox"
+                title={"task-" + index}
+                id={"task-" + index}
+              />
+              <label htmlFor={"task-" + index}></label>
+              <span className="task-text">{todo}</span>
+            </li>
           ))}
         </ul>
-        <div className="add-todo">
-          <input
-            type="text"
-            value={newTodo}
-            placeholder="Add Todo"
-            onChange={(e) => setNewTodo(e.target.value)}
-          />
-          <button
-            onClick={handleAddTodo}
-            type="button"
-            title="Add Todo"
-          ></button>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
